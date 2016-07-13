69f1fa76f639f8b514e393f39da3f07d51cdcb68
Matheus Piscioneri <mxczpiscioneri@gmail.com>
2016-07-13 01:52:05 -0300

    Created class Game

---
+++ Model/GameClass.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * @author Matheus Pisiconeri
+ */
+class GameClass {
+
+    private $id;
+    private $start_time;
+    private $end_time;
+    private $total_kills;
+    private $players = array();
+    private $kills = array();
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getStart_time() {
+        return $this->start_time;
+    }
+
+    public function getEnd_time() {
+        return $this->end_time;
+    }
+
+    public function getTotal_kills() {
+        return $this->total_kills;
+    }
+
+    public function getPlayers() {
+        return $this->players;
+    }
+
+    public function getKills() {
+        return $this->kills;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function setStart_time($start_time) {
+        $this->start_time = $start_time;
+    }
+
+    public function setEnd_time($end_time) {
+        $this->end_time = $end_time;
+    }
+
+    public function setTotal_kills($total_kills) {
+        $this->total_kills = $total_kills;
+    }
+
+    public function setPlayers($players) {
+        $this->players[] = $players;
+    }
+
+    public function setKills($kills) {
+        $this->kills[] = $kills;
+    }
+
+    public function addKill() {
+        $this->setTotal_kills($this->getTotal_kills() + 1);
+    }
+
+    public function toJSON() {
+        return json_encode(get_object_vars($this));
+    }
+
+}
