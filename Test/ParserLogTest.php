1b9ee409dc5ed2bc4d336d3e42b5e9f324260d7e
Matheus Piscioneri <mxczpiscioneri@gmail.com>
2016-07-15 02:01:49 -0300

    Add more tests, total kills not null, add kill, total kills equal total means

--- Test/ParserLogTest.php
+++ Test/ParserLogTest.php
@@ -23,4 +23,47 @@ class ParserLogTest extends PHPUnit_Framework_TestCase {
         $this->assertNotEmpty(file_get_contents($this->file_name));
     }
 
+    public function testTotalKillsNotIsNull() {
+        require_once '../Model/GameClass.php';
+        $game = new GameClass();
+
+        //kill = 0
+        $this->assertEquals(0, $game->getTotal_kills());
+    }
+
+    public function testAddKill() {
+        require_once '../Model/GameClass.php';
+        $game = new GameClass();
+
+        //kill = 1
+        $game->addKill();
+        $this->assertEquals(1, $game->getTotal_kills());
+
+        //kill = 2
+        $game->addKill();
+        $this->assertEquals(2, $game->getTotal_kills());
+    }
+
+    public function testTotalKillsEqualsTotalMeans() {
+        require_once '../Model/GameClass.php';
+        require_once '../Controller/ParserLog.php';
+
+        $parser = new ParserLog($this->file_name);
+        $games = $parser->getJsonGame();
+
+        foreach ($games as $data) {
+            $item = json_decode($data);
+            foreach ($item as $game) {
+                //get total_kills
+                $total_kills = $game->total_kills;
+                $total_means = 0;
+                foreach ($game->means as $means) {
+                    //get total_means
+                    $total_means += $means;
+                }
+            }
+        }
+        $this->assertEquals($total_kills, $total_means);
+    }
+
 }
